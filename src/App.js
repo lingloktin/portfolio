658fe6c5af4e67fe9997b9bad286babefc204c79
LltQ <lingloktin@gmail.com>
2024-04-12 01:47:07 +0800

    4. Projects.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
+import { Experience } from "./components/Experience";
+import { Projects } from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Experience />
+      <Projects />
     </div>
   );
 }
