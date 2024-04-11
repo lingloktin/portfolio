b205cf577e7b3d74cf2164dc74e3a9ec0b16ea5b
LltQ <lingloktin@gmail.com>
2024-04-12 01:46:47 +0800

    custom data

---
+++ src/constants/experienceData.js
@@ -0,0 +1,39 @@
+const experienceData = [
+    {
+        year: "2023 Aug - Current",
+        shortName: "sth",
+        title: "St. Teresa's Hospital",
+        subtitle: "Programmer",
+        description: [
+            "React Native expertise for mobile app enhancement",
+            "Skilled in MSSQL for accurate data management",
+            "ASP.NET maintenance ensuring seamless patient experiences",
+            "Proficient in VB programming for program optimization",
+            "Provided effective technical support & played a key role in UAT testing"
+        ]
+    },
+    {
+        year: "2022 Jul - 2022 Sep",
+        shortName: "moveit",
+        title: "Move It Move It Limited",
+        subtitle: "Full Stack Engineer Intern",
+        description: [
+            "Developed full-stack web and mobile apps using Flutter",
+            "Contributed to designing and implementing new tech features",
+            "Collaborated with cross-functional teams to deliver high-quality software solutions"
+        ]
+    },
+    {
+        year: "2021 May - 2021 Sep",
+        shortName: "pks",
+        title: "Peanut King Solution Limited",
+        subtitle: "Software Engineer Intern",
+        description: [
+            "Collaborated cross-departmentally in software development",
+            "Created interactive websites using Bootstrap",
+            "Developed robotic codes for educational purposes"
+        ]
+    }
+];
+
+export default experienceData;
