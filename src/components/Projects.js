46360262a24fdf95215e01987ee69a38d708288d
LltQ <lingloktin@gmail.com>
2024-04-12 01:45:20 +0800

    Create Projects.js

---
+++ src/components/Projects.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export const Projects = () => {
+    return (
+        <section className="projects" id='projects'>
+            <Container></Container>
+        </section>
+    )
+}
