71b9fc3f7d4d4726f7a11bc3a77ef81c711a2534
LltQ <lingloktin@gmail.com>
2024-04-29 01:28:34 +0800

    redesigned experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,82 +4,90 @@ import educationData from "../constants/educationData";
 import experienceData from "../constants/experienceData";
 
 export const Experience = () => {
-    return (
-        <section className="experience">
+  return (
+    <section className="experience">
+      <Container>
+        <h2 className="section-header" id="experience">
+          My <span>Journey</span>
+        </h2>
+      </Container>
+      <Container>
+        <Row>
+          <Col>
             <Container>
-                <h2 className="section-header" id="experience">My <span>Journey</span></h2>
+              <Row className="aligh-items-center experience-row">
+                <Col>
+                  <h3 className="title">Education</h3>
+                  <div className="experience-box">
+                    {educationData.map((education, index) => (
+                      <div className="experience-content" key={index}>
+                        <div className="content">
+                          <div className="year">
+                            <i className="bx bxs-calendar"></i> {education.year}
+                          </div>
+                          <Row>
+                            <Col>
+                              <div
+                                className={`${education.shortName}-icon`}
+                              ></div>
+                            </Col>
+                            <Col className={`${education.shortName}-title`}>
+                              <h3>{education.title}</h3>
+                              <h4>{education.subtitle}</h4>
+                            </Col>
+                          </Row>
+                          <Row>
+                            <p>{education.description}</p>
+                          </Row>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </Col>
+              </Row>
             </Container>
+          </Col>
+          <Col>
             <Container>
-                <Row>
-                    <Col>
-                        <Container>
-                            <Row className="aligh-items-center experience-row">
-                                <Col>
-                                    <h3 className="title">Education</h3>
-                                    <div className="experience-box">
-                                        {educationData.map((education, index) => (
-                                            <div className="experience-content" key={index}>
-                                                <div className="content">
-                                                    <div className="year"><i className="bx bxs-calendar"></i> {education.year}</div>
-                                                    <Row>
-                                                        <Col>
-                                                            <div className={`${education.shortName}-icon`}></div>
-                                                        </Col>
-                                                        <Col className={`${education.shortName}-title`}>
-                                                            <h3>{education.title}</h3>
-                                                            <h4>{education.subtitle}</h4>
-                                                        </Col>
-                                                    </Row>
-                                                    <Row>
-                                                        <p>{education.description}</p>
-                                                    </Row>
-                                                </div>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Container>
-                    </Col>
-                    <Col>
-                        <Container>
-                            <Row className="aligh-items-center experience-row">
-                                <Col>
-                                    <h3 className="title">Experience</h3>
-                                    <div className="experience-box">
-                                        {experienceData.map((experience, index) => (
-                                            <div className="experience-content" key={index}>
-                                                <div className="content">
-                                                    <div className="year"><i className="bx bxs-calendar"></i> {experience.year}</div>
-                                                    <Row>
-                                                    <Col>
-                                                            <div className={`${experience.shortName}-icon`}></div>
-                                                        </Col>
-                                                        <Col className={`${experience.shortName}-title`}>
-                                                            <h3>{experience.title}</h3>
-                                                            <h4>{experience.subtitle}</h4>
-                                                        </Col>
-                                                    </Row>
-                                                    <Row>
-                                                        <ul>
-                                                            {experience.description.map((item, idx) => (
-                                                                <li key={idx}>{item}</li>
-                                                            ))}
-                                                        </ul>
-                                                    </Row>
-                                                    
-                                                </div>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Container>
-                    </Col>                
-                </Row>
+              <Row className="aligh-items-center experience-row">
+                <Col>
+                  <h3 className="title">Experience</h3>
+                  <div className="experience-box">
+                    {experienceData.map((experience, index) => (
+                      <div className="experience-content" key={index}>
+                        <div className="content">
+                          <div className="year">
+                            <i className="bx bxs-calendar"></i>{" "}
+                            {experience.year}
+                          </div>
+                          <Row>
+                            <Col>
+                              <div
+                                className={`${experience.shortName}-icon`}
+                              ></div>
+                            </Col>
+                            <Col className={`${experience.shortName}-title`}>
+                              <h3>{experience.title}</h3>
+                              <h4>{experience.subtitle}</h4>
+                            </Col>
+                          </Row>
+                          <Row>
+                            <ul>
+                              {experience.description.map((item, idx) => (
+                                <li key={idx}>{item}</li>
+                              ))}
+                            </ul>
+                          </Row>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </Col>
+              </Row>
             </Container>
-            
-        </section>
-        
-    )
-}
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
