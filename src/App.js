f138b2c119c2e9dc39cb4a62fb20ab8408e031fc
lingloktin <marcus.ling.lt@gmail.com>
2024-05-07 17:48:42 +0800

    Skills.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Experience } from "./components/Experience";
 import { Projects } from "./components/Projects";
+import { Skills } from "./components/Skills";
 
 function App() {
   useEffect(() => {
@@ -17,6 +18,7 @@ function App() {
       <Banner />
       <Experience />
       <Projects />
+      <Skills />
     </div>
   );
 }
