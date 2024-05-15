2a815ce611677aa06cfac776e4b8c33e9c59685a
LltQ <lingloktin@gmail.com>
2024-05-16 02:03:08 +0800

    added Footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Experience } from "./components/Experience";
 import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
 import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 function App() {
   useEffect(() => {
@@ -22,6 +23,7 @@ function App() {
       <Projects />
       <Skills />
       <Contact />
+      <Footer />
     </div>
   );
 }
