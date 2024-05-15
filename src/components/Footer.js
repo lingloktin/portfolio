3cb0a61fddae5621cfa5de239f53449fb382c02d
LltQ <lingloktin@gmail.com>
2024-05-16 02:03:02 +0800

    created Footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+
+export const Footer = () => {
+    return (
+        <footer>
+            <Row>
+                <Col className="text-center py-3">Copyright &copy; 2024 by Marcus Ling | All Rights Reserved.</Col>
+            </Row>
+        </footer>
+    );
+}
