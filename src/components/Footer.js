348bf7dd6673c91179a15794466c223afeda8e3d
lingloktin <marcus.ling.lt@gmail.com>
2024-05-16 17:48:50 +0800

    updated footer layout and color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 
 export const Footer = () => {
     return (
-        <footer>
-            <Row>
-                <Col className="text-center py-3">Copyright &copy; 2024 by Marcus Ling | All Rights Reserved.</Col>
+        <footer className="footer">
+            <Row className="footer-text">
+                Copyright &copy; 2024 by Marcus Ling | All Rights Reserved.
             </Row>
         </footer>
     );
